e5ba3c6f9d18bab92c9ab1d9e2b8e74ef533d6f0
VergilKellley <verg_1@yahoo.com>
2024-08-31 16:53:01 -0500

    first commit as php file

---
+++ index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- boxicons -->
+    <link
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+    <!-- favicon cross-browser -->
+    <link rel="shortcut icon" href="images/color_running_logo.png" />
+
+    <!-- favicon IOS devices -->
+    <link rel="apple-touch-icon" href="images/color_running_logo.png" />
+    <title>Hair Salon</title>
+  </head>
+  <body>
+    <section id="home">
+      <nav>
+        <a class="nav-logo" href="/">Logo</a>
+        <div>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="about">About</a>
+            </li>
+            <li>
+              <a href="services">Services</a>
+            </li>
+            <li>
+              <a href="contact">Contact</a>
+            </li>
+            <li>
+              <a href="/">Login</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <a class="book-appointment-btn" href="#">Book Appointment</a>
+        </div>
+      </nav>
+    </section>
+    <!-- HERO -->
+    <section id="header">
+      <div class="first-container">
+        <img src="uploads/large-stars.png" alt="" />
+        <p>Lorem ipsum dolor</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <span></span>
+        <div id="social-icons-container">
+          <p>follow Us</p>
+          <span class="instagram-svg"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path><circle cx="16.806" cy="7.207" r="1.078"></circle><path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path></svg></span>
+          <span class="facebook-svg"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path></svg></span>
+        </div>
+      </div>
+      <div class="large-hero-img">
+        <img src="uploads/pic-4.png" alt="" />
+      </div>
+      <div id="right-side-header">
+        <h1><span class="secondary-color">Styles </span>that make</h1>
+        <h1>you <span class="secondary-color">Feel</span> like</h1>
+        <h1>a <span class="secondary-color">Star</span></h1>
+        <p style="margin-top: 25px">Lorem ipsum dolor sit, .</p>
+        <p>Lorem ipsum dolor sit amet consectetur .</p>
+        <div
+          style="
+            margin-top: 50px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 1rem;
+          "
+        >
+          <p style="color: #333; font-size: 1.7rem; font-weight: 600">
+            Checkout Our Services
+          </p>
+          <a href="#"
+            ><img src="uploads/down-arrow-alt-regular-24.png" alt=""
+          /></a>
+        </div>
+      </div>
+      <div id="header-reviews-container">
+        <h3>4.5</h3>
+        <span></span>
+        <div>
+          <img src="uploads/star-solid-24.png" alt="" />
+          <img src="uploads/star-solid-24.png" alt="" />
+          <img src="uploads/star-solid-24.png" alt="" />
+          <img src="uploads/star-solid-24.png" alt="" />
+          <img src="uploads/star-half-solid-24.png" alt="" />
+          <p>310 Reviews</p>
+        </div>
+      </div>
+    </section>
+
+    <!-- SIX SERVICES BOXES -->
+    <section id="section-two">
+      <div class="six-boxes-container">
+        <div class="top-boxes">
+          <div class="box-one">
+            <img src="uploads/hair-style.png" alt="">
+            <p>Hair Styles</p>
+          </div>
+          <div class="box-two">
+            <img src="uploads/waxing.png" alt="">
+            <p>Waxing</p>
+          </div>
+          <div class="box-three">
+            <img src="uploads/makeup.png" alt="">
+            <p>Makeup</p>
+          </div>
+        </div>
+        <div class="bottom-boxes">
+          <div class="box-one">
+            <img src="uploads/eyebrow-threading.png" alt="">
+            <p>Eyebrow Threading</p>
+          </div>
+          <div class="box-two">
+            <img src="uploads/manicure.png" alt="">
+            <p>Manicure</p>
+          </div>
+          <div class="box-three">
+            <img src="uploads/pedicure.png" alt="">
+            <p>Pedicure</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- THREE IMAGES -->
+    <section id="sec-three" class="section-container section-three-container">
+      <div style="width: 50%;">
+      <div id="section-three-left-side">
+        <div style="width: 300px; height: 50vh; display: flex; position: relative; align-items: flex-end;margin-top: 30px; border-bottom: 30px solid var(--secondary-color); background-color: var(--secondary-color); left:30px">
+          <img style="position:absolute; width:300px; left:-30px;" src="uploads/pic-11.jpg" alt="">
+        </div>
+        <div style="height: 35vh; width:100%; display: flex; flex-direction: column; justify-content: center; align-items: center; margin-top: 10vh;">
+          <img style="width: 75%;" src="uploads/pic-6.png" alt="">
+        </div>
+        <div>
+          <div style="display: flex;justify-content: center; align-items: flex-end;background-color:var(--background-color); position: relative; height: 38vh; margin-top:200px; border:30px solid var(--secondary-color); width: 90%">
+            <img style="width:100%; position: absolute;" src="uploads/pic-14.png" alt="">
+          </div>
+          <div id="section-three-large-text">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <h2>Lorem ipsum dolor sit.</h2>
+            <h2>Lorem, ipsum dolor.</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+      <div id="section-three-right-side">
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <p>Lorem, ipsum dolor.</p>
+          <div style="position:relative; display: flex; justify-content: center;align-items: flex-end; height:35%; width: 80%; margin-top:55%; margin-bottom:20vh; border:30px solid var(--secondary-color);background-color: var(--background-color);">
+            <img style="position:absolute; width:100%;" src="uploads/pic-3.png" alt="">
+          </div>
+          <div style="height: 400px; display: flex; justify-content: center; align-items: center; padding: 20px;">
+            <div style="text-align: center;">
+              <h2>Lorem ipsum dolor sit amet.</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, nisi. Earum saepe expedita ipsam, eum porro consectetur inventore ea itaque?</p>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <!-- MOST POPULAR SERVICES -->
+    <section id="sec-four" class="section-container sec-four">
+      <div style="padding-top: 200px;line-height: 1.5;margin-bottom: 20px;">
+        <h3 style="font-size: var(--h3-font-size);">Most Popular Services</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div style="display: flex;">
+      <div style="display: flex;gap: 1rem; flex-wrap: wrap;">
+      <div class="popular-services-container">
+        <div>
+          <div>
+            <div class="popular-services">
+              <img src="uploads/hair-style.png" alt="">
+            <h3>Hair Styles</h3>
+            <ul>
+              <li>Shampoo</li>
+              <li>Cut & Style</li>
+            </ul>
+            <p>$180</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="popular-services-container">
+        <div>
+          <div>
+            <div class="popular-services">
+              <img src="uploads/manicure.png" alt="">
+            <h3>Manicure</h3>
+            <ul>
+              <li>Cuticles</li>
+              <li>Color & Art</li>
+            </ul>
+            <p>$120</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="popular-services-container">
+        <div>
+          <div>
+            <div class="popular-services">
+              <img src="uploads/makeup.png" alt="">
+              <div>
+                <h3>Makeup</h3>
+                <ul>
+                  <li>Consultation</li>
+                  <li>Make-over</li>
+                </ul>
+              </div>
+              <p>$160</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="popular-services-container">
+        <div>
+          <div>
+            <div class="popular-services">
+              <img src="uploads/pedicure.png" alt="">
+              <div>
+                <h3>Pedicure</h3>
+                <ul>
+                  <li><span>Lufa</span></li>
+                  <li>Trim</li>
+                </ul>
+              </div>
+              <p>$140</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+      </div>  
+      <div id="services-lg-img" style="width: 100%; display: flex; justify-content: flex-end; align-items: flex-end;margin-top: -500px; ">
+          <img style="width: 40%; position:relative;" src="uploads/pic-16.jpg" alt="">
+      </div>
+      <div style="display: flex;position: absolute;bottom: 7vh;gap: 1rem;">
+        <div>
+          <a style=" border: 1px solid var(--primary-color); background-color: var(--primary-color); padding: 10px 20px;border-radius: 30px; color: #fff;" href="#">
+            Book Appointment
+          </a>
+        </div>
+        <div>
+          <a style=" border: 1px solid var(--seconday-color); background-color: var(--secondary-color); padding: 10px 20px;border-radius: 30px; color: #fff;" href="#">
+            View All Services
+          </a> 
+        </div>     
+      </div>
+    </div>
+    </section>
+
+    <!-- ALL SERVICES -->
+     <section class="section-container" id="all-services">
+        <div style="display:flex;">
+          <div style="width:fit-content;">   
+            <div>
+              <img style="width:100%" src="uploads/pic-5.png" alt="">
+            </div>
+          </div>
+          <div>
+            <div style="width:100%; margin-bottom: 2rem; line-height: 1.5;">
+              <p>Here you can...</p>
+              <h2>Checkout our services...</h2>
+              <div style="display: flex; align-items: center; gap: 1rem;">
+                <h2>and when you're ready </h2>
+                <a style="border: 1px solid var(--primary-color); background-color: var(--primary-color); padding: 10px 20px;border-radius: 30px; color: #fff;" href="book_appointment_4.php">Book Appointment</a>
+              </div>
+              
+            </div>
+            <div class="services-list-container" style="height:400px; overflow-y: scroll;border:2px solid var(--primary-color); border-radius: .7rem; padding:10px">
+            <div>
+              <h3>Manicure</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, esse nam eius animi quod neque molestias libero rem qui placeat.</p>
+              <p>$120</p>
+            </div>
+            <div>
+              <h3>Manicure</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, esse nam eius animi quod neque molestias libero rem qui placeat.</p>
+              <p>$120</p>
+            </div>
+            <div>
+              <h3>Manicure</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, esse nam eius animi quod neque molestias libero rem qui placeat.</p>
+              <p>$120</p>
+            </div>
+            <div>
+              <h3>Manicure</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, esse nam eius animi quod neque molestias libero rem qui placeat.</p>
+              <p>$120</p>
+            </div>
+            <div>
+              <h3>Manicure</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, esse nam eius animi quod neque molestias libero rem qui placeat.</p>
+              <p>$120</p>
+            </div>
+            <div>
+              <h3>Manicure</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, esse nam eius animi quod neque molestias libero rem qui placeat.</p>
+              <p>$120</p>
+            </div>
+            <div>
+              <h3>Manicure</h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, esse nam eius animi quod neque molestias libero rem qui placeat.</p>
+              <p>$120</p>
+            </div>
+          </div>
+          </div>
+        </div>
+     </section>
+  <!-- </body>
+</html> -->
+<?php
+require_once 'footer.php';
+?>
