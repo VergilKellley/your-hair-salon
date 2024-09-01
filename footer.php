fcbd55c8f9b450b7e83bc20c60c8a6f92ab584bb
VergilKellley <verg_1@yahoo.com>
2024-08-31 20:04:10 -0500

    added php variables for footer

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
+<?php
+  require_once 'header.php';
+?>
+
+
 <!-- NAVIGATION BEFORE FOOTER -->
-<section style='background: <?= $primary_color; ?>' class="navBeforeFooter">
+<section style=' display:flex; background-color:var(--primary-color)' class="navBeforeFooter">
       <div class="box">
         <div><h2 style='font-size:2.3rem'><?= $business_name; ?></h2></div>
-        <div class="bf-text">Sign Up For Special Offers</div>
+        <div>Sign Up For Special Offers</div>
         <p class="para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore quas eaque pariatur consequuntur quos.</p>
 
         <div class="bf-text">Subscribe For Offers</div>
@@ -18,10 +23,9 @@
           <li><a href="index.php">Home</a></li>
           <li><a href="about.php">About</a></li>
           <!-- <li><a href="Services.php">Services</a></li> -->
-          <li><a href="Portfolio.php">Gallery</a></li>
-          <li><a href="Contact.php">Contact</a></li>
+          <li><a href="services.php">Sevices</a></li>
+          <li><a href="contact.php">Contact</a></li>
         </ul>
-
         <div class="icons">
           <a href="#" class="fa-brands fa-facebook-f"></a>
           <a href="#" class="fa-brands fa-instagram"></a>
@@ -32,7 +36,7 @@
       <div class="box">
         <div class="bf-text">Say Hi!</div>
         <ul class="sayHiItem">
-          <li><a href="mailto:contact@yourbarbershop.com"><?= $email; ?></a></li>
+          <li><a href="mailto:contact@yourbarbershop.com" rel="noopener noreferrer"><?= $business_email; ?></a></li>
           <!-- <li>
             <a href="">contact@yoursalon.com</a>
           </li> -->
@@ -40,26 +44,26 @@
 
         <div class="bf-text">Call Us</div>
         <ul class="sayHiItem">
-          <li><a href="tel:+19803351233"><?= $phone; ?></a></li>
+          <li><a href="tel:+19803351233" rel="noopener noreferrer"><?= $business_phone; ?></a></li>
         </ul>
 
         <div class="bf-text">Visit Us</div>
         <a href="contact.php#google-map" style='color:#fff; text-decoration: none'>
-          <p><?= $street_address; ?></p>
-          <p><?= $suite_unit; ?></p>
-        <p><?= $city; ?>, <?= $business_state; ?> <?= $zip; ?></p></a>
+          <p><?= $business_street_address; ?></p>
+        <p><?= $business_city; ?>, <?= $business_state; ?> <?= $business_zip; ?></p></a>
         
       </div>
      </section>
 
-    <footer style='background:<?= $primary_color; ?>'>
+    <footer style='background:var(--primary-color); display:flex'>
       <div class="fbox">
         Copyright &copy; 2024 <?= $business_name; ?> | Website designed and powered by
-        <a style='color:#fff; text-decoration:none' href="https://webqwick.com/" target='_blank'>WebQwick.com</a>.
+        <a style='color:#fff; text-decoration:none' href="https://webqwick.com/" target='_blank' rel="noopener noreferrer">WebQwick.com</a>.
       </div>
       <div>
-        All other trademarks, service marks and trade names referenced in this
-        material are the property of their respective owner.
+        <p style="color:#fff">All other trademarks, service marks and trade names referenced in this
+        material are the property of their respective owner.</p>
+        
       </div>
     </footer>
   </body>
